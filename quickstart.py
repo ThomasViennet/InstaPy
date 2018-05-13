b6a12faed24345581df07b1fde97b572e3da2b6b
Thomas <viennet.t@gmail.com>
2018-05-13 15:59:57 +0200

    quickstart good

--- quickstart.py
+++ quickstart.py
@@ -1,13 +1,15 @@
 import os
 import time
+import schedule
 from tempfile import gettempdir
 
 from selenium.common.exceptions import NoSuchElementException
 
 from instapy import InstaPy
 
-insta_username = ''
-insta_password = ''
+insta_username = 'chatoune_france'
+insta_password = 'bT1k;Q-Wm'
+
 
 # set headless_browser=True if you want to run InstaPy on a server
 
@@ -21,36 +23,54 @@ session = InstaPy(username=insta_username,
                   headless_browser=False,
                   multi_logs=True)
 
-try:
-    session.login()
+def job():
+    try:
+        session.login()
 
-    # settings
-    session.set_relationship_bounds(enabled=True,
-				 potency_ratio=-1.21,
-				  delimit_by_numbers=True,
-				   max_followers=4590,
-				    max_following=5555,
-				     min_followers=45,
-				      min_following=77)
-    session.set_do_comment(True, percentage=10)
-    session.set_comments(['aMEIzing!', 'So much fun!!', 'Nicey!'])
-    session.set_dont_include(['friend1', 'friend2', 'friend3'])
-    session.set_dont_like(['pizza', 'girl'])
+        # settings
+        session.set_relationship_bounds(enabled=True,
+    				 potency_ratio=-1.21,
+    				  delimit_by_numbers=True,
+    				   max_followers=4590,
+    				    max_following=5555,
+    				     min_followers=45,
+    				      min_following=77)
+        session.set_user_interact(amount=4, randomize=True, percentage=100)
+        session.set_do_follow(enabled=True, percentage=100, times=2)
+        session.set_do_like(enabled=True, percentage=100)
+        session.set_do_comment(True, percentage=25)
+        session.set_comments(['Je suis fan @{} !', 'Magnifique', 'Superbe', 'Top !', 'Ton compte instagram est vraiment top @{} !'])
 
-    # actions
-    session.like_by_tags(['natgeo'], amount=1)
+        # actions
+        session.interact_user_followers(['miaoubox'], amount=10, randomize=True)
+        session.unfollow_users(amount=10, onlyInstapyFollowed = True, onlyInstapyMethod = 'FIFO', sleep_delay=600, unfollow_after=4*24*60*60)
 
-except Exception as exc:
-    # if changes to IG layout, upload the file to help us locate the change
-    if isinstance(exc, NoSuchElementException):
-        file_path = os.path.join(gettempdir(), '{}.html'.format(time.strftime('%Y%m%d-%H%M%S')))
-        with open(file_path, 'wb') as fp:
-            fp.write(session.browser.page_source.encode('utf8'))
-        print('{0}\nIf raising an issue, please also upload the file located at:\n{1}\n{0}'.format(
-            '*' * 70, file_path))
-    # full stacktrace when raising Github issue
-    raise
+    except Exception as exc:
+        # if changes to IG layout, upload the file to help us locate the change
+        if isinstance(exc, NoSuchElementException):
+            file_path = os.path.join(gettempdir(), '{}.html'.format(time.strftime('%Y%m%d-%H%M%S')))
+            with open(file_path, 'wb') as fp:
+                fp.write(session.browser.page_source.encode('utf8'))
+            print('{0}\nIf raising an issue, please also upload the file located at:\n{1}\n{0}'.format(
+                '*' * 70, file_path))
+        # full stacktrace when raising Github issue
+        raise
 
-finally:
-    # end the bot session
-    session.end()
+    finally:
+        # end the bot session
+        session.end()
+
+schedule.every().day.at("8:33").do(job)
+schedule.every().day.at("9:41").do(job)
+schedule.every().day.at("11:02").do(job)
+schedule.every().day.at("12:17").do(job)
+schedule.every().day.at("13:38").do(job)
+schedule.every().day.at("16:30").do(job)
+schedule.every().day.at("18:24").do(job)
+schedule.every().day.at("19:46").do(job)
+schedule.every().day.at("21:24").do(job)
+schedule.every().day.at("22:55").do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
